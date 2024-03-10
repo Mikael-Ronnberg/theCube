d54cb8f882390632b38c5e3425fcb04bf35d1c4e
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2024-03-10 18:44:08 +0100

    testing agan

--- src/components/theView/theViewStyles.ts
+++ src/components/theView/theViewStyles.ts
@@ -10,8 +10,4 @@ export const ViewContainer = styled.div`
 
 export const CanvasContainer = styled(Canvas)`
   background: black;
-  @media (orientation: portrait) {
-    @media (-webkit-device-pixel-ratio: 3) {
-    }
-  }
 `;
